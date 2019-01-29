b55702fec6b14c282a93b1273b0e1079d8ac37dd
Bobby3620 <41356385+Bobby3620@users.noreply.github.com>
2019-01-28 19:07:06 -0500

    ay

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,35 @@
 package com.company;
+import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner kbInput = new Scanner(System.in);
+        System.out.println("Choose to enter a color by using rgb or hexadecimal");
+        String chose = kbInput.nextLine();
+
+        if (chose.equalsIgnoreCase("rgb")) {
+            //Need a for loop to set all values
+            //Add constraint x > 0 & x < 256
+            int[] rgb = new int[3];
+            System.out.println("Please enter 3 values between 0-255. If value is over the constraint, the program just stops.");
+            int constraint1 = -1, constraint2 = 256;
+            int nums = 1; //This allows the while loop to be true for the first time
+            int timer = -1; //Will terminate the while loop also
+
+            // I hate error messages
+            while (nums > constraint1 && nums < constraint2 && timer < 3) {
+                nums = kbInput.nextInt();
+                for (int i = 0; i < rgb.length; i++) {
+                    rgb[i] = nums;
+                }
+                timer++;
+            }
+
+            if (chose.equalsIgnoreCase("hexadecimal")) {
+
+            }
+        }
     }
 }
